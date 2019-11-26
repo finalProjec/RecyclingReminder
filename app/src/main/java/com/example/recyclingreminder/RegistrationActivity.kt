ec69b1d34217c4c584dc2c75544cfa431fdbc5f1
Naman Mittal <nmittal2@umd.edu>
2019-11-26 16:58:44 -0500

    FixedFirebaseAuthentication

--- app/src/main/java/com/example/recyclingreminder/RegistrationActivity.kt
+++ app/src/main/java/com/example/recyclingreminder/RegistrationActivity.kt
@@ -12,8 +12,8 @@ import android.widget.Toast
 
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
-//import com.google.firebase.auth.AuthResult
-//import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 
 class RegistrationActivity : AppCompatActivity() {
@@ -22,51 +22,51 @@ class RegistrationActivity : AppCompatActivity() {
     private var passwordTV: EditText? = null
     private var regBtn: Button? = null
     private var progressBar: ProgressBar? = null
-//
-//    private var mAuth: FirebaseAuth? = null
+
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-//
-//        mAuth = FirebaseAuth.getInstance()
-//
+
+        mAuth = FirebaseAuth.getInstance()
+
         initializeUI()
-//
-//        regBtn!!.setOnClickListener { registerNewUser() }
+
+        regBtn!!.setOnClickListener { registerNewUser() }
     }
-//
-//    private fun registerNewUser() {
-//        progressBar!!.visibility = View.VISIBLE
-//
-//        val email: String
-//        val password: String
-//        email = emailTV!!.text.toString()
-//        password = passwordTV!!.text.toString()
-//
-//        if (TextUtils.isEmpty(email)) {
-//            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
-//            return
-//        }
-//        if (TextUtils.isEmpty(password)) {
-//            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
-//            return
-//        }
-//
-//        mAuth!!.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener { task ->
-//                if (task.isSuccessful) {
-//                    Toast.makeText(applicationContext, "Registration successful!", Toast.LENGTH_LONG).show()
-//                    progressBar!!.visibility = View.GONE
-//
-//                    val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
-//                    startActivity(intent)
-//                } else {
-//                    Toast.makeText(applicationContext, "Registration failed! Please try again later", Toast.LENGTH_LONG).show()
-//                    progressBar!!.visibility = View.GONE
-//                }
-//            }
-//    }
-//
+
+    private fun registerNewUser() {
+        progressBar!!.visibility = View.VISIBLE
+
+        val email: String
+        val password: String
+        email = emailTV!!.text.toString()
+        password = passwordTV!!.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Registration successful!", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
+
+                    val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(applicationContext, "Registration failed! Please try again later", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
+                }
+            }
+    }
+
     private fun initializeUI() {
         emailTV = findViewById(R.id.email)
         passwordTV = findViewById(R.id.password)
